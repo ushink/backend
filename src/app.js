ad810a0b3ead244361a375b0b49e8aa71f39a8a5
Ulyana <ushinkevich@mail.ru>
2023-11-18 18:42:18 +0400

    Написан обработчик запросов

--- src/app.js
+++ src/app.js
@@ -1,8 +1,34 @@
 const http = require("http");
 const getUsers = require("./modules/users");
 
-const server = http.createServer((request, response) => {
-  if (request.url === "/users") {
+const server = http.createServer(async (request, response) => {
+  // Написать обработчик запроса:
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const query = url.searchParams;
+  const name = query.get("hello");
+
+  if (query.has("hello")) {
+    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
+    if (name) {
+      response.status = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: text/plain";
+      response.write(`Hello, ${name}`);
+      response.end();
+
+      return;
+    }
+    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
+    response.status = 400;
+    response.header = "Content-Type: text/plain";
+    response.write("Enter a name");
+    response.end();
+
+    return;
+  }
+
+  // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
+  if (query.has("users")) {
     response.status = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: application/json";
@@ -11,14 +37,21 @@ const server = http.createServer((request, response) => {
 
     return;
   }
+  // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
+  if (request.url === "/") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: text/plain";
+    response.write("Hello, word!");
+    response.end();
 
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello, word!");
+    return;
+  }
+  // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  response.status = 500;
   response.end();
 });
 
-server.listen(3000, () => {
+server.listen(3003, () => {
   console.log("Сервер запущен");
 });
