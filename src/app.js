5a3baf88e8f1842a50b02717b7c0ba11849ec3b3
Ulyana <ushinkevich@mail.ru>
2023-11-21 16:32:52 +0400

    --fix status

--- src/app.js
+++ src/app.js
@@ -10,18 +10,14 @@ const server = http.createServer(async (request, response) => {
   if (query.has("hello")) {
     // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
     if (name) {
-      response.status = 200;
-      response.statusMessage = "OK";
-      response.header = "Content-Type: text/plain";
+      response.writeHead(200, { "Content-Type": "text/plain" });
       response.write(`Hello, ${name}`);
       response.end();
 
       return;
     }
     // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // response.status = 400;
-    // response.header = "Content-Type: text/plain";
-    response.writeHead(400, {'Content-Type': 'text/plain'});
+    response.writeHead(400, { "Content-Type": "text/plain" });
     response.write("Enter a name");
     response.end();
 
@@ -30,9 +26,7 @@ const server = http.createServer(async (request, response) => {
 
   // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
   if (query.has("users")) {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.writeHead(200, { "Content-Type": "text/plain" });
     response.write(getUsers());
     response.end();
 
@@ -40,17 +34,14 @@ const server = http.createServer(async (request, response) => {
   }
   // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
   if (request.url === "/") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
+    response.writeHead(200, { "Content-Type": "text/plain" });
     response.write("Hello, word!");
     response.end();
 
     return;
   }
   // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-  // response.status = 500;
-  response.writeHead(500, {'Content-Type': 'text/plain'});
+  response.writeHead(500, { "Content-Type": "text/plain" });
   response.end();
 });
 
